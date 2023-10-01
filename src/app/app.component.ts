c091d24f8f52bf0b98bf7ee0d0aa698fba940e99
33dew <kaudekbog@gmail.com>
2023-10-01 20:34:44 +0200

    some ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+type Page = {
+  title: string;
+  path: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zadaniedom';
+
+  pages: Page[] = [
+    { title: 'Counter', path: '/counter' },
+    { title: 'Todo', path: '/todo' },
+    { title: 'Form', path: '/form' }
+  ]
 }
